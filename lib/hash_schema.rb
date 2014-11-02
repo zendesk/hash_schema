65221a20db80fcc93b7de52c7b54a44251c8e024
Po Chen <pchen@zendesk.com>
2014-11-02 23:37:42 +1100

    Copy paste code

--- lib/hash_schema.rb
+++ lib/hash_schema.rb
@@ -1,5 +1,188 @@
 require "hash_schema/version"
 
 module HashSchema
-  # Your code goes here...
+  class Schema
+    def initialize(chain = nil)
+      @chain = chain
+    end
+
+    def interpret(data)
+      (@interpretations = []).tap do |_|
+        interpret_errors(validate(data), 'root')
+      end
+    end
+
+    attr_reader :chain
+
+    def pretty_validate(data)
+      require 'json'
+      JSON.pretty_generate(validate(data))
+    end
+
+    def expectation
+      self.class.name.split('::').last.sub('Schema', '')
+    end
+
+    protected
+
+    def error(data)
+      expect(expectation, data)
+    end
+
+    def expect(wanted, unwanted)
+      "Expected #{wanted} but got #{unwanted.inspect}"
+    end
+
+    private
+
+    def interpret_errors(errors, *prefixes)
+      *prefixes, prefix = prefixes
+      prefixes <<= format(errors, prefix)
+
+      case errors
+        when Hash
+          errors.each do |key, val|
+            next if val.nil?
+            interpret_errors(val, *prefixes, key)
+          end
+        when Array
+          errors.each_with_index do |val, index|
+            next if val.nil?
+            interpret_errors(val, *prefixes, index)
+          end
+        when String
+          @interpretations << (prefixes << errors).join(' > ')
+      end
+    end
+
+    def format(error, name)
+      if name.kind_of?(Numeric)
+        name = "##{name}"
+        return name if error.is_a?(String)
+      end
+
+      case error
+        when Hash
+          "#{name}:{}"
+        when Array
+          "#{name}:[]"
+        when String
+          ".#{name}"
+      end
+    end
+  end
+
+  class OptionalSchema < Schema
+    def validate(data)
+      return if data.is_a?(Void)
+      return chain.validate(data) if chain.kind_of?(Schema)
+      expect(chain.inspect, data) unless chain == data
+    end
+  end
+
+  class OrSchema < Schema
+    def initialize(*schemas)
+      @chain = schemas
+    end
+
+    def validate(data)
+      chain.each do |schema|
+        if data.is_a?(Array) && schema.is_a?(ArraySchema) || data.is_a?(Hash) && schema.is_a?(HashSchema)
+          return schema.validate(data)
+        end
+        return if schema.validate(data).nil?
+      end
+      error(data)
+    end
+
+    def expectation
+      *names, name = chain.map(&:expectation)
+      names.join(', ') << " or #{name}"
+    end
+  end
+
+  class StringSchema < Schema
+    def validate(data)
+      return if data.is_a?(String)
+      error(data)
+    end
+  end
+
+  class NumberSchema < Schema
+    def validate(data)
+      return if data.kind_of?(Numeric)
+      error(data)
+    end
+  end
+
+  class BooleanSchema < Schema
+    def validate(data)
+      return if data.is_a?(TrueClass) || data.is_a?(FalseClass)
+      error(data)
+    end
+  end
+
+  class ArraySchema < Schema
+    def validate(data)
+      return data.map { |item| chain.validate(item) } if data.is_a?(Array)
+      error(data)
+    end
+
+    def expectation
+      "[#{chain.expectation}]"
+    end
+  end
+
+  class EnumSchema < Schema
+    def initialize(*enum)
+      @chain = enum
+    end
+
+    def validate(data)
+      return if chain.include?(data)
+      error(data)
+    end
+
+    def expectation
+      *vals, val = chain.map(&:inspect)
+      vals.join(', ') << " or #{val}"
+    end
+  end
+
+  class HashSchema < Schema
+    def initialize(strict: false, **hash)
+      @chain = hash
+      @strict = strict
+    end
+
+    def validate(hash)
+      return error(hash) unless hash.is_a?(Hash)
+      {}.tap do |output|
+        chain.each do |key, schema|
+          val = hash[key] || hash.fetch(key.to_s, Void.new)
+          output[key] = if schema.kind_of?(Schema)
+            schema.validate(val)
+          else
+            schema == val ? nil : expect(schema.inspect, val)
+          end
+        end
+
+        output.merge!((hash.keys - chain.keys).map { |k| [k, unexpected(k)] }.to_h) if @strict
+      end
+    end
+
+    def unexpected(key)
+      "Unexpected key: #{key.to_s.inspect}"
+    end
+  end
+
+  private
+
+  class Void
+    def to_s
+      'Nothing'
+    end
+
+    alias :inspect :to_s
+  end
 end
---
+++ spec/hash_schema_spec.rb
@@ -0,0 +1,232 @@
+require 'spec_helper'
+
+describe HashSchema do
+  Testers = ['abc123', 123, true, HashSchema::Void.new]
+
+  def except(type)
+    Testers.reject { |tester| tester.is_a? type }
+  end
+
+  describe HashSchema::StringSchema do
+    subject { HashSchema::StringSchema }
+
+    it 'passes for String type value' do
+      expect(subject.new.validate('abc123!@#$%^&*()_=-+')).to be_nil
+    end
+
+    it 'fails for other types' do
+      except(String).each do |tester|
+        expect(subject.new.validate(tester)).to_not be_nil
+      end
+    end
+  end
+
+  describe HashSchema::NumberSchema do
+    subject { HashSchema::NumberSchema }
+
+    it 'passes for Numeric type value' do
+      expect(subject.new.validate(1234567890)).to be_nil
+    end
+
+    it 'fails for other types' do
+      except(Numeric).each do |tester|
+        expect(subject.new.validate(tester)).to_not be_nil
+      end
+    end
+  end
+
+  describe HashSchema::BooleanSchema do
+    subject { HashSchema::BooleanSchema }
+
+    it 'passes for Boolean type value' do
+      expect(subject.new.validate(true)).to be_nil
+    end
+
+    it 'fails for other types' do
+      except(TrueClass).each do |tester|
+        expect(subject.new.validate(tester)).to_not be_nil
+      end
+    end
+  end
+
+  describe HashSchema::OptionalSchema do
+    subject { HashSchema::OptionalSchema }
+
+    it 'passes for Void type value' do
+      expect(subject.new.validate(HashSchema::Void.new)).to be_nil
+    end
+
+    context 'when initialized with a Schema' do
+      it 'delegates to the inner Schema' do
+        schema = Class.new(HashSchema::Schema).new
+
+        expect(schema).to receive(:validate)
+
+        subject.new(schema).validate(1)
+      end
+    end
+
+    context 'when initialized with literal value' do
+      it 'passes for the exact value' do
+        expect(subject.new(1).validate(1)).to be_nil
+      end
+
+      it 'fails for different value' do
+        expect(subject.new(1).validate(2)).to_not be_nil
+      end
+    end
+  end
+
+  describe HashSchema::EnumSchema do
+    subject { HashSchema::EnumSchema.new(1, 'a', true) }
+
+    it 'passes for provided literal values' do
+      expect(subject.validate(1)).to be_nil
+    end
+
+    it 'passes for provided literal values' do
+      expect(subject.validate('a')).to be_nil
+    end
+
+    it 'passes for provided literal values' do
+      expect(subject.validate(true)).to be_nil
+    end
+
+    it 'fails for other values' do
+      expect(subject.validate(2)).to_not be_nil
+    end
+
+    it 'fails for other values' do
+      expect(subject.validate('b')).to_not be_nil
+    end
+
+    it 'fails for other values' do
+      expect(subject.validate(false)).to_not be_nil
+    end
+  end
+
+  describe HashSchema::OrSchema do
+    subject { HashSchema::OrSchema }
+
+    let(:multitype) do
+      subject.new(
+        HashSchema::NumberSchema.new,
+        HashSchema::StringSchema.new,
+        HashSchema::ArraySchema.new(HashSchema::StringSchema.new),
+        HashSchema::HashSchema.new
+      )
+    end
+
+    it 'passes if any inner schema passes' do
+      expect(multitype.validate(1)).to be_nil
+    end
+
+    it 'passes if any inner schema passes' do
+      expect(multitype.validate('a')).to be_nil
+    end
+
+    context 'when having inner ArraySchema or HashSchema' do
+      it 'delegates to given ArraySchema for array' do
+        expect(multitype.validate([])).to be_a(Array)
+      end
+
+      it 'delegates to given HashSchema for hash' do
+        expect(multitype.validate({})).to be_a(Hash)
+      end
+    end
+
+    it 'fails if all inner schema fail' do
+      expect(multitype.validate(false)).to_not be_nil
+    end
+  end
+
+  describe HashSchema::ArraySchema do
+    subject { HashSchema::ArraySchema }
+
+    let(:string_array) { ['a', 'b', 'c', 'd'] }
+    let(:chaos_array) { ['a', 1, false, [], {}] }
+    let(:string_array_schema) { subject.new(HashSchema::StringSchema.new) }
+    let(:hash_array_schema) { subject.new(HashSchema::HashSchema.new) }
+
+    it 'passes for array of the given type' do
+      expect(string_array_schema.validate(string_array).compact).to eq([])
+    end
+
+    it 'fails for non-array input' do
+      expect(string_array_schema.validate(123)).to_not be_nil
+    end
+
+    it 'handles nested structure' do
+      expect(hash_array_schema.validate([{}, { x: 1 }, { y: 2 }])).to eq([{}, {}, {}])
+    end
+
+    it 'fails for array containing values not being the given type' do
+      expect(string_array_schema.validate(chaos_array).compact.size).to eq(4)
+    end
+  end
+
+  describe HashSchema::HashSchema do
+    subject { HashSchema::HashSchema }
+
+    it 'passes if all literal attributes pass' do
+      schema = subject.new(
+        string: HashSchema::StringSchema.new,
+        number: HashSchema::NumberSchema.new,
+        boolean: HashSchema::BooleanSchema.new,
+        nothing: HashSchema::OptionalSchema.new,
+        enum: HashSchema::EnumSchema.new(1, 'a', true)
+      )
+
+      data = {
+        string: 'abc',
+        number: 123,
+        boolean: true,
+        enum: 'a'
+      }
+
+      expect(schema.validate(data).reject { |_, v| v.nil? }).to be_empty
+    end
+
+    it 'handles nested structure' do
+      schema = subject.new(
+        hash: subject.new(
+          literal: 1
+        )
+      )
+
+      data = {
+        hash: {
+          literal: 1
+        }
+      }
+
+      expect(schema.validate(data)).to eq({ hash: { literal: nil } })
+    end
+
+    it 'handles nested structure' do
+      schema = subject.new(
+        array: HashSchema::ArraySchema.new(HashSchema::BooleanSchema.new)
+      )
+
+      data = {
+        array: [true, false]
+      }
+
+      expect(schema.validate(data)).to eq({ array: [nil, nil] })
+    end
+
+    it 'fails for non-hash input' do
+      expect(subject.new.validate(123)).to_not be_nil
+    end
+
+    context 'when strict mode' do
+      it 'fails for extra attributes' do
+        schema = subject.new(strict: true, x: 0)
+
+        data = { x: 0, y: 1 }
+
+        expect(schema.validate(data).reject { |_, v| v.nil? }).to include(:y)
+      end
+    end
+  end
+end
